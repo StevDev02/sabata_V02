bebb85dd27b19b80dc0c1ec32a46661149781fc5
StevDev02 <stevdev04@gmail.com>
2024-05-05 18:20:42 -0500

    developer: add vercel server for job in team, resolve to bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
+// Aqui va el import para vercel
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
