7aeb88da81412152cf79dbf3f1e8291acd5ae516
StevDev02 <stevdev04@gmail.com>
2024-05-24 23:32:24 -0500

    dv-StevDev: Actualizacion para el manejo de errores desde la rama de StevDev

--- src/components/Home/Nav_Home/LogoHome.tsx
+++ src/components/Home/Nav_Home/LogoHome.tsx
@@ -4,7 +4,7 @@ function LogoHome() {
         <div className='flex z-50 items-center gap-5'>
     <a className='tracking-[5px]'>SABATA</a>
     <div className='w-[2px] h-[15px] bg-white  ' />
-    <p className='tracking-[5px] text-[14px] '>INICIO</p>
+    <p className='tracking-[5px] text-[14px] '>StevDev</p>
   </div>
      );
 }
